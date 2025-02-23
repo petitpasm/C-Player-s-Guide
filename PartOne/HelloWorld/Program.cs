84a109f7dbe04b820a2071c7b5f2aa262055901f
petitpasm <petitpasm@icloud.com>
2025-02-23 17:13:00 +0100

    did it better that time

---
+++ PartOne/HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello fellas.");
+Console.WriteLine("To respond to the question: How many statements a program can handle?");
+Console.WriteLine("I'd say that it depends on the computer, more power, more possibilities.");
