8ca4f1c092384db89959dd441c628eba18e37c27
petitpasm <petitpasm@icloud.com>
2025-02-23 17:51:37 +0100

    level 5 finished

---
+++ PartOne/ThingNamer3000/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); /*Stores the kind of item*/
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine(); //Stores the description of said item.
+string c = " of Doom";
+string d = "3000";
+Console.WriteLine("The " + b + " " + a + c + " " + d + "!");
+
+//To make this code more readable, I suggest to get rid of "c" and "d" variables
+//and rename "a" and "b" to be more understandable. Like so:
+
+Console.WriteLine("What kind of thing are we talking about?");
+string kind = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string description = Console.ReadLine();
+Console.WriteLine("The " + description + " " + kind + " of Doom 3000!");
+
+//We get it faster and we can read this later without lost infos.
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # C-Player-s-Guide
+
 My personal trip in the Realm of C#.
+
+Based on "C# Player's Guide" book.
