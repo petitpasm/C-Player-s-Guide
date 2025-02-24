0428ec5f44e3af16a9dd0977b2fb8c9bae4edb7f
petitpasm <petitpasm@icloud.com>
2025-02-24 21:55:41 +0100

    finished level 7

---
+++ PartOne/VariableShop/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+byte a = 255;
+sbyte b = 127;
+short c = 32767;
+ushort d = 65535;
+int e = 2_147_483_647;
+uint f = 4_294_967_295U;
+long g = 9_223_372_036_854_775_807L;
+ulong h = 18_446_744_073_709_551_615UL;
+float i = 1999.12f;
+double j = 234.23425;
+decimal k = 234.235463612431532552m;
+string l = "This is a string";
+char m = 'd';
+bool n = true;
+
+Console.WriteLine(a + " is max for a 'byte'");
+Console.WriteLine(b + " is max for a 'sbyte'");
+Console.WriteLine(c + " is max for a 'short'");
+Console.WriteLine(d + " is max for a 'ushort'");
+Console.WriteLine(e + " is max for a 'int'");
+Console.WriteLine(f + " is max for a 'uint'");
+Console.WriteLine(g + " is max for a 'long'");
+Console.WriteLine(h + " is max for a 'ulong'");
+Console.WriteLine(i);
+Console.WriteLine(j);
+Console.WriteLine(k);
+Console.WriteLine(l);
+Console.WriteLine(m);
+Console.WriteLine(n);
